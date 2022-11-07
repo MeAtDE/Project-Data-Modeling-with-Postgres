3bd8b82423be9af5af5f2b8433f486508cf371a7
MeAtDE <117223230+MeAtDE@users.noreply.github.com>
2022-11-07 17:55:33 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,35 +1,36 @@
+#etl.py connect to Sparkify database, extracts and processes the log_data and #song_data, and loads data into the five tables.
+
 import os
 import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
 
-
 def process_song_file(cur, filepath):
-    # open song file
+     #open song file
     df = pd.read_json(filepath, lines=True) 
 
-    # insert song record
+    """insert song record"""
     song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
-    # insert artist record
+    """insert artist record"""
     artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude',
                       'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
-    # open log file
+    """open log file"""
     df = pd.read_json(filepath,lines=True)
 
-    # filter by NextSong action
+    """filter by NextSong action"""
     df = df[df['page'] == 'NextSong'] 
 
-    # convert timestamp column to datetime
+    """convert timestamp column to datetime"""
     t = pd.to_datetime(df['ts'], unit='ms')
     
-    # insert time data records
+    """insert time data records"""
     time_data     = [t, t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday]
     column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
     time_df       = pd.DataFrame.from_dict(dict(zip(column_labels, time_data))) 
@@ -37,17 +38,17 @@ def process_log_file(cur, filepath):
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
-    # load user table
+    """load user table"""
     user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']] 
 
-    # insert user records
+    """insert user records"""
     for i, row in user_df.iterrows():
         cur.execute(user_table_insert, row)
 
-    # insert songplay records
+    """insert songplay records"""
     for index, row in df.iterrows():
         
-        # get songid and artistid from song and artist tables
+        """get songid and artistid from song and artist tables"""
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
@@ -57,23 +58,23 @@ def process_log_file(cur, filepath):
         else:
             songid, artistid = None, None
 
-        # insert songplay record
+        """insert songplay record"""
         songplay_data = (row.ts,row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 def process_data(cur, conn, filepath, func):
-    # get all files matching extension from directory
+    """get all files matching extension from directory"""
     all_files = []
     for root, dirs, files in os.walk(filepath):
         files = glob.glob(os.path.join(root,'*.json'))
         for f in files :
             all_files.append(os.path.abspath(f))
 
-    # get total number of files found
+    """get total number of files found"""
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
 
-    # iterate over files and process
+    """iterate over files and process"""
     for i, datafile in enumerate(all_files, 1):
         func(cur, datafile)
         conn.commit()
@@ -81,12 +82,14 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """connect to the db"""
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
-
+    
+    """close the db"""
     conn.close()
 
 
